81f4687d8daa606cf1497d86767c7b10447b6e07
minus-1 <sgone2842@GMAIL.COM>
2026-07-03 15:03:02 +0530

    feat: integrate api-gateway in auth and user service
    - Added api-gateway to docker compose
    - Removed direct http access to internal services
    - Externalized constants from auth and user service for easier integration with api-gateway

--- auth-service/src/main/java/com/ezhome/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/ezhome/authservice/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth/v1")
 public class AuthController {
 
   private final AuthService authService;
