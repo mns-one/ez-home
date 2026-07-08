f78cb65f44e148314f30ca5f7db2a1a5ce1e2e8d
minus-1 <sgone2842@GMAIL.COM>
2026-07-08 12:46:10 +0530

    feat: integrate device-registry-service
    - Added device-registry-service route to api-gateway
    - Enabled header verification in device-registry controller
    - Updated docker-compose for device-registry-service
    - Added dummy accounts in auth and user service for testing

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -6,3 +6,23 @@ CREATE TABLE users (
   passwordhash        VARCHAR(255),
   role                VARCHAR(255)
 );
+
+INSERT INTO "users" (id, email, passwordhash, role)
+SELECT '223e4567-e89b-12d3-a456-426614174006', 'user@test.com',
+       '$2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'USER'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM "users"
+    WHERE id = '223e4567-e89b-12d3-a456-426614174006'
+       OR email = 'user@test.com'
+);
+
+INSERT INTO "users" (id, email, passwordhash, role)
+SELECT '223e4567-e89b-12d3-a456-426614174008', 'admin@test.com',
+       '$2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'ADMIN'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM "users"
+    WHERE id = '223e4567-e89b-12d3-a456-426614174008'
+       OR email = 'admin@test.com'
+);
--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -9,3 +9,20 @@ CREATE TABLE users (
   created_at            TIMESTAMPTZ     NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO "users" (id, email, username, city, profile_image_url)
+SELECT '223e4567-e89b-12d3-a456-426614174006', 'user@test.com', 'user', 'city1', 'xyz.com'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM "users"
+    WHERE id = '223e4567-e89b-12d3-a456-426614174006'
+       OR email = 'user@test.com'
+);
+
+INSERT INTO "users" (id, email, username, city, profile_image_url)
+SELECT '223e4567-e89b-12d3-a456-426614174008', 'admin@test.com', 'user', 'city1', 'xyz.com'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM "users"
+    WHERE id = '223e4567-e89b-12d3-a456-426614174008'
+       OR email = 'admin@test.com'
+);
