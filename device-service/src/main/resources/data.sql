ef4297bacb9a839617e95493b542d3bfed07064a
minus-1 <sgone2842@GMAIL.COM>
2026-07-09 12:45:03 +0530

    feat: add gRPC flow for loading device ids to ingestion-service
    On startup ingestion-service now loads all device ids in redis from device-service for Telemetry request validation. It is implemented using gRPC communication to device-service.
    
    Changes made to both service repos:
    - Added gRPC client and server file
    - Added proto file
    - Implemented fetchDeviceIdPage in device-service service layer to fetch all device ids via pagination
    - Added dummy data for testing the flow to device-service data.sql

--- device-service/src/main/resources/data.sql
+++ device-service/src/main/resources/data.sql
@@ -9,3 +9,12 @@ CREATE TABLE devices (
   alerts_enabled        BOOLEAN         NOT NULL DEFAULT FALSE,
   created_at            TIMESTAMPTZ     NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO "devices" (id, serial_no, user_id, model_name, custom_name)
+SELECT '223e4567-e89b-12d3-a456-426614174006', 'SMT5001', '223e4567-e89b-12d3-a456-426614174006', '4gang', 'door';
+
+INSERT INTO "devices" (id, serial_no, user_id, model_name, custom_name)
+SELECT '223e4567-e89b-12d3-a456-426614174007', 'SMT5002', '223e4567-e89b-12d3-a456-426614174006', '4gang', 'door';
+
+INSERT INTO "devices" (id, serial_no, user_id, model_name, custom_name)
+SELECT '223e4567-e89b-12d3-a456-426614174008', 'SMT5003', '223e4567-e89b-12d3-a456-426614174006', '4gang', 'door';
---
+++ ingestion-service/src/main/java/com/ezhome/ingestionservice/grpc/DeviceServiceGrpcClient.java
@@ -0,0 +1,57 @@
+package com.ezhome.ingestionservice.grpc;
+
+import java.util.Iterator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.ezhome.ingestionservice.redis.DeviceIdCache;
+
+import device.DeviceServiceGrpc;
+import device.StreamDeviceIdsRequest;
+import device.StreamDeviceIdsResponse;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Service
+@Slf4j
+public class DeviceServiceGrpcClient {
+
+    private final DeviceServiceGrpc.DeviceServiceBlockingStub blockingStub;
+    private final DeviceIdCache deviceIdCache;
+
+    public DeviceServiceGrpcClient(
+        @Value("${device.service.address}") String serverAddress,
+        @Value("${device.service.grpc.port}") int serverPort,
+        DeviceIdCache deviceIdCache
+    ) {
+
+        log.info("Connecting to device-registry-service gRPC {}:{}", serverAddress, serverPort);
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort)
+                .usePlaintext()
+                .build();
+
+        blockingStub = DeviceServiceGrpc.newBlockingStub(channel);
+        this.deviceIdCache = deviceIdCache;
+
+    }
+
+    public void streamAllDeviceIds(int pageSize) {
+
+        StreamDeviceIdsRequest request = StreamDeviceIdsRequest.newBuilder()
+            .setPageSize(pageSize)
+            .build();
+
+        Iterator<StreamDeviceIdsResponse> responses = blockingStub.streamAllDeviceIds(request);
+        log.info("fetching ids....");
+        while (responses.hasNext()) {
+            deviceIdCache.loadDevices(responses.next().getSerialNosList());
+        }
+
+    }
+    
+}
