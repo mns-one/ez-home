8c7355b649cb7ddbfca43e9aa76ec47ee624b32b
minus-1 <sgone2842@GMAIL.COM>
2026-07-09 12:30:27 +0530

    feat: add ingestion-service core logic
    - Designed HTTP endpoints
    - Implemented entity, exception, service, redis package
    - Defined redis and error handling logic

---
+++ ingestion-service/src/main/java/com/ezhome/ingestionservice/redis/DeviceIdCache.java
@@ -0,0 +1,58 @@
+package com.ezhome.ingestionservice.redis;
+
+import java.util.List;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DeviceIdCache {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public DeviceIdCache(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void addDeviceId(String deviceId) {
+
+        redisTemplate
+                .opsForSet()
+                .add("device_id_set", deviceId);
+    }
+
+
+    public boolean exists(String deviceId) {
+
+        Boolean exists =
+                redisTemplate
+                    .opsForSet()
+                    .isMember(
+                        "device_id_set",
+                        deviceId
+                    );
+
+        return Boolean.TRUE.equals(exists);
+    }
+
+
+    public void removeDeviceId(String deviceId) {
+
+        redisTemplate
+                .opsForSet()
+                .remove("device_id_set", deviceId);
+    }
+
+    public void loadDevices(List<String> ids) {
+
+        if (ids == null || ids.isEmpty()) {
+            return;
+        }
+
+        redisTemplate
+            .opsForSet()
+            .add("device_id_set", ids.toArray(new String[0]));
+    }
+    
+}
